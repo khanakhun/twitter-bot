02c022195ffd59bdd990ab6c7f68aed68015ae1e
khanakhun <littelhunter2@gmail.com>
2021-09-01 02:54:01 -0700

    make it beter

--- downloadDm.js
+++ downloadDm.js
@@ -1,55 +1,8 @@
-const axios = require('axios')
 const config = require('dotenv').config()
-const TOKEN = process.env.TWITTER_BEARER_TOKEN
-const needle = require('needle')
-const apiUrl = 'https://api.twitter.com/1.1/direct_messages/events/list.json'
-const Twitter = require('twitter')
-
-// const getDms = async () => {
-// 	const response = await axios.get(apiUrl, {
-// 		headers: {
-// 			Authorization: `Bearer ${TOKEN}`,
-// 		},
-// 	})
-// 	console.log(response.data)
-// }
-// // getDms()
-// async function getRules() {
-// 	const response = await needle('get', apiUrl, {
-// 		headers: {
-// 			Authorization: `OAuth oauth_consumer_key="${process.env.API_KEY}" Bearer ${TOKEN}`,
-// 			'Content-type': 'application/json',
-// 		},
-// 	})
-// 	console.log(response.body)
-// 	return response.body
-// }
-// getRules()
-
-// var client = new Twitter({
-// 	consumer_key: process.env.API_KEY,
-// 	consumer_secret: process.env.API_SECRET,
-// 	access_token_key: process.env.ACCESS_TOKEN,
-// 	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// })
-
-// var params = { screen_name: '@khanakhun1' }
-// client.get(
-// 	'direct_messages/events/list',
-// 	params,
-// 	function (error, tweets, response) {
-// 		if (!error) {
-// 			console.log(tweets)
-// 			console.log(response)
-// 		}
-// 	},
-// )
-
-// Twit
-
-var Twit = require('twit')
-
-var T = new Twit({
+const moment = require('moment')
+const Twit = require('twit')
+const fs = require('fs')
+const T = new Twit({
 	consumer_key: process.env.API_KEY,
 	consumer_secret: process.env.API_SECRET,
 	access_token: process.env.ACCESS_TOKEN,
@@ -58,45 +11,75 @@ var T = new Twit({
 	strictSSL: true, // optional - requires SSL certificates to be valid.
 })
 
-//
-//  tweet 'hello world!'
-//
+var obj = {
+	dms: [],
+}
+// Function to Parse unxi timestamp
+const parseDate = async (date) => {
+	let formatedDate = await moment(parseInt(date)).format('MM/DD/YYYY')
 
-//
-//  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//
-T.get(
-	'direct_messages/events/list',
+	return formatedDate
+}
 
-	function (err, data, response) {
-		const events = data.events
-		let name
-		let message
-		events.map((ele) => {
-			T.get(
-				'/users/lookup',
-				{ user_id: ele.message_create.sender_id },
-
-				function (err, data, response) {
-					name = data[0].screen_name
-					console.log(name)
-					// console.log(data[0].screen_name)
-				},
-			)
-			message = ele.message_create.message_data.text
-
-			console.log(message)
+// function to fetch twitter User data by id
+const getUser = (id) => {
+	return new Promise((resolve, reject) => {
+		let params = { user_id: id }
+		T.get('/users/lookup', params, function (err, data, response) {
+			if (err) {
+				reject(err)
+			}
+			resolve([data[0].screen_name, data[0].name, data[0].id_str])
 		})
-	},
-)
+	})
+}
 
-//
-//  get the list of user id's that follow @tolga_tezel
-//
-// T.get(
-// 	'followers/ids',
-// 	{ screen_name: '@khanakhun1' },
-// 	function (err, data, response) {
-// 		console.log(data)
-// 	},
-// )
+// function to save DMs in Json file
+// const collection = path.resolve(__dirname, './history.json')
+const saveFile = (sender, name, message, id, formatedDate) => {
+	try {
+		obj.dms.push({
+			sender: sender,
+			screen_name: name,
+			message: message,
+			id: id,
+			date: formatedDate,
+		})
+		var json = JSON.stringify(obj)
+		var fs = require('fs')
+		fs.writeFile('Dms.json', json, 'utf8', (err) => {
+			if (err) {
+				return err
+			}
+		})
+	} catch (error) {
+		throw error
+	}
+}
+// function to fetch Dm of user
+const fectchDms = async () => {
+	const dms = T.get(
+		'direct_messages/events/list',
+		async function (err, data, response) {
+			const events = data.events
+			let name
+			let message
+			events.map(async (ele) => {
+				name = await getUser(ele.message_create.sender_id)
+				message = ele.message_create.message_data.text
+				let formatedDate = await parseDate(
+					ele.created_timestamp,
+				)
+				saveFile(
+					name[1],
+					name[0],
+					message,
+					name[2],
+					formatedDate,
+				)
+			})
+			console.log('Complete.....')
+		},
+	)
+}
+fectchDms()
